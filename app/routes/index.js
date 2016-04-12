a8041888922ba2e7955f2ff9ef8c41d36851a34e
Abigail Rolling and Brad London <london.brad@gmail.com>
2016-04-12 16:06:27 -0700

    signed in user authentication with new user duplicate checking

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,13 +1,31 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model(){
+    return this.store.findAll('user');
+  },
 
   actions: {
     newUser(params){
-      var newPerson = this.store.createRecord('user', params);
-      console.log(newPerson);
-      newPerson.save();
+      var model = this.currentModel;
+      var duplicate = false;
+      model.forEach(function(user){
+        if(params.username === user.get('username')){
+          alert("That username already has an account");
+          duplicate = true;
+        }
+      });
+      if (!duplicate){
+        var newPerson = this.store.createRecord('user', params);
+        console.log(newPerson);
+        newPerson.save();
+      }
       this.transitionTo('index');
+    },
+
+    signIn(params){
+      console.log(params);
+      this.transitionTo('profile', params);
     }
   }
 });
--- app/services/who-am-i.js
+++ app/services/who-am-i.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Service.extend({
+  user: {username: "Guest", id: 0},
+
+  logIn(user){
+    this.set('user', user);
+  },
+
+  logOut(){
+    this.set('user', {username: "Guest", id: 0});
+  }
+
 });
