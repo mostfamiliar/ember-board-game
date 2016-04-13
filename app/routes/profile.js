62aa47e0382be28e17a1498b949ce8facb0c1d9a
Brad London and Jessica Fix <london.brad@gmail.com>
2016-04-13 10:52:35 -0700

    Loop through users games and save its attributes to game model in DB

--- app/routes/profile.js
+++ app/routes/profile.js
@@ -11,24 +11,48 @@ export default Ember.Route.extend({
       localUser: this.store.query('user', {
         orderBy: 'username',
         equalTo: params.username
-      }) //this is a freaking array with one object
+      }) //this is an array with one object
     });
   },
 
+//make a list of games in db in model
   actions: {
-    addGame(params){
+    addToGameList(params){
+      var self = this;
       var model = this.currentModel;
       var foundRecord = false;
-      model.localUser.forEach(function(game){
-        if(game.get('objectid') === params.gameId){
-          foundRecord = true;
+      params.forEach(function(gameid){
+        // run through database list of games, not user's list of games
+        model.gameList.forEach(function(game){
+          if(game.get('objectid') === gameid){
+            foundRecord = true;
+          }
+        });
+        if (foundRecord === false){
+          var gameUrl = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20xml%20where%20url%20%3D%20'http%3A%2F%2Fwww.boardgamegeek.com%2Fxmlapi2%2Fthing%3Fid%3D" + gameid + "'&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
+          console.log(gameUrl);
+
+          Ember.$.getJSON(gameUrl).then(function(responseJSON){
+            var response = responseJSON.query.results.items.item;
+            console.log(response.name[0].value);
+            var attributes = {
+              gameId: response.id,
+              title: response.name[0].value,
+              image: response.image,
+              description: response.description,
+              published: response.yearpublished.value,
+              maxPlayers: response.maxplayers.value
+            };
+            var newGame = self.store.createRecord('game', attributes);
+            newGame.save();
+            //newGame.get('ownedBy').addObject[0](localUser)
+            //.then (adding rentals/cities)
+            console.log(newGame);
+
+          });
+
         }
       });
-      if (foundRecord === false){
-        var newGame = this.store.createRecord('game', params);
-        newGame.save();
-        console.log(newGame.title);
-      }
     }
   }
 });
