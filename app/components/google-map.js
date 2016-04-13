05c770fd3a68f3246228e2b1d5522c229667970f
Abigail Rolling and Torrence Stratton <arolling@gmail.com>
2016-04-13 13:28:17 -0700

    small tweaks

--- app/components/google-map.js
+++ app/components/google-map.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   map: Ember.inject.service('google-map'),
-  showMap: Ember.on('didInsertElement', function(){
+  showMap: Ember.on('didInsertElement', 'didUpdateAttrs', function(){
     var user = this.get('user');
     var container = this.$('.map-display')[0];
     var options = {
