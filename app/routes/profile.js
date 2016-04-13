c6a55db7aad48de3a0e7e18574e672eccfa52dcc
Brad London and Jessica Fix <london.brad@gmail.com>
2016-04-13 13:11:26 -0700

    add willing list

--- app/routes/profile.js
+++ app/routes/profile.js
@@ -16,26 +16,28 @@ export default Ember.Route.extend({
     });
   },
 
-//make a list of games in db in model
   actions: {
     addToGameList(params){
       var self = this;
       var model = this.currentModel;
       var foundRecord = false;
+      var foundGame;
       params.forEach(function(gameid){
         // run through database list of games, not user's list of games
         model.databaseGames.forEach(function(game){
-          if(game.get('objectid') === gameid){
+          console.log(game.get("gameId"));
+          console.log(gameid);
+          if(game.get('gameId') === gameid){
             foundRecord = true;
+            foundGame = game;
           }
         });
         if (foundRecord === false){
           var gameUrl = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20xml%20where%20url%20%3D%20'http%3A%2F%2Fwww.boardgamegeek.com%2Fxmlapi2%2Fthing%3Fid%3D" + gameid + "'&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
-          // console.log(gameUrl);
 
           Ember.$.getJSON(gameUrl).then(function(responseJSON){
             var response = responseJSON.query.results.items.item;
-            // console.log(response.name[0].value);
+
             var attributes = {
               gameId: response.id,
               title: response.name[0].value,
@@ -47,15 +49,19 @@ export default Ember.Route.extend({
             var newGame = self.store.createRecord('game', attributes);
             var user =model.localUser.objectAt(0);
             newGame.get('ownedBy').addObject(user);
+            newGame.get('willingPlayers').addObject(user);
             newGame.save().then(function(){
               return user.save();
             });
-
-            //.then (adding rentals/cities)
             console.log(newGame);
-
           });
-
+        } else {
+          var user = model.localUser.objectAt(0);
+          foundGame.get('ownedBy').addObject(user);
+          foundGame.get('willingPlayers').addObject(user);
+          foundGame.save().then(function(){
+            return user.save();
+          });
         }
       });
     }
