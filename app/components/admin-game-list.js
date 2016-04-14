bc8cb505d03d435e79a5564cf1f0b656c6807641
Brad London <london.brad@gmail.com>
2016-04-14 12:00:15 -0700

    Buggy add to your collection button

--- app/components/admin-game-list.js
+++ app/components/admin-game-list.js
@@ -4,8 +4,12 @@ export default Ember.Component.extend({
   addGame: [],
   sortedGames: Ember.computed.sort('games', 'sortDefinition'),
   sortDefinition: ['title'],
-
+  addDbGames: false,
   actions: {
+    addDbGames(){
+      this.set('addDbGames', true);
+    },
+
     addToWants(){
       // console.log(this.get('addGame'));
       this.sendAction("addToWantList", this.get('addGame'));
