c25dc0fef95e1e2539ae760466063d0d0f77efa4
abby rolling and torrence stratton <arolling@gmail.com>
2016-04-14 11:58:14 -0700

    log distance on callback

--- app/components/google-map.js
+++ app/components/google-map.js
@@ -14,26 +14,26 @@ export default Ember.Component.extend({
   }),
 
   showMap: Ember.on('didInsertElement', 'didUpdateAttrs', 'showDistance', function(){
-    var users = this.get('users');
-    for(var i = 0; i <= this.get('users.length'); i++) {
-      var user = this.get('users').objectAt(i);
-      console.log(user);
-    }
-    var browser = this.get('whoAmI');
-    var container = this.$('.map-display')[0];
-    var options = {
-      center: this.get('map').center(user.get('lat'), user.get('lng')),
-      zoom: 15
-    };
-    var userLocation = user.get('userLocation');
-    console.log(userLocation);
-    var newMap = this.get('map').findMap(container, options);
-    this.get('map').placeMarker(newMap, userLocation);
-    if(this.get('showDistance')){
-      var browserLocation = browser.get('userLocation');
-      this.get('map').placeMarker(newMap, browserLocation);
-      newMap.setZoom(11);
-    }
+    // for(var i=0; i < this.get('users.length'); i++){
+    //     var eachUser = this.get('users').objectAt(i);
+    //
+
+      var user = this.get('user');
+      var browser = this.get('whoAmI');
+      var container = this.$('.map-display')[0];
+      var options = {
+        center: this.get('map').center(user.get('lat'), user.get('lng')),
+        zoom: 15
+      };
+      var userLocation = user.get('userLocation');
+      var newMap = this.get('map').findMap(container, options);
+      this.get('map').placeMarker(newMap, userLocation);
+      if(this.get('showDistance')){
+        var browserLocation = browser.get('userLocation');
+        this.get('map').placeMarker(newMap, browserLocation);
+        newMap.setZoom(11);
+      }
+    // }
   }),
   actions: {
 
--- app/components/group-display.js
+++ app/components/group-display.js
@@ -4,18 +4,18 @@ export default Ember.Component.extend({
   whoAmI: Ember.inject.service(),
   showDistance: false,
   findUser: null,
-  userList: Ember.computed('users', function(){
-    for(var i = 0; i <= this.get('users.length'); i++) {
-       var eachUser = this.get('users').objectAt(i);
-             this.set('findUser', eachUser);
-            console.log(eachUser);
-
-
-    }
-                return eachUser;
-  }),
+  // userList: Ember.computed('users', function(){
+  //   for(var i = 0; i <= this.get('users.length'); i++) {
+  //      var eachUser = this.get('users').objectAt(i);
+  //           console.log(eachUser);
+  //
+  //
+  //   }
+  //               return eachUser;
+  // }),
   actions: {
-    showDistance(){
+    showDistance(user){
+      this.set('findUser', user);
       this.set('showDistance', true);
     }
   }
