7cb34190b38255fbf107cf61143e6d91029d81f5
brad london and torrence stratton <london.brad@gmail.com>
2016-04-15 08:31:56 -0700

    add group title and group list to specific group page

--- app/routes/group.js
+++ app/routes/group.js
@@ -2,10 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   whoAmI: Ember.inject.service(),
-  model(){
+  model(params){
     return Ember.RSVP.hash({
       whoAmI: this.get('whoAmI'),
-      users: this.store.findAll('user')
+      users: this.store.findAll('user'),
+      group: this.store.findRecord('group', params.group_id)
     });
   }
 });
