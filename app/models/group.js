c70543e6a22eedc47c4ffdc07a781766342a6f18
abby rolling and torrence stratton <tlstratton@gmail.com>
2016-04-14 18:11:53 -0700

    Filter users by location

--- app/models/group.js
+++ app/models/group.js
@@ -2,5 +2,5 @@ import Model from 'ember-data/model';
 
 export default Model.extend({
   members: DS.hasMany('user', {inverse:"groups"}, {async:true}),
-  gamesMatched: DS.hasMany('game', {inverse:null}, {async:true})
+  gamesMatched: DS.belongsTo('game', {inverse:null}, {async:true})
 });
