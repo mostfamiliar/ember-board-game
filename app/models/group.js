7c5cfef0f1b918e9526c39dbc5292b4bc311ec19
Brad London <london.brad@gmail.com>
2016-04-14 17:57:13 -0700

    add group display styling on group page and admin page

--- app/models/group.js
+++ app/models/group.js
@@ -1,6 +1,7 @@
 import Model from 'ember-data/model';
 
 export default Model.extend({
+  title: DS.attr(),
   members: DS.hasMany('user', {inverse:"groups"}, {async:true}),
   gamesMatched: DS.hasMany('game', {inverse:null}, {async:true})
 });
